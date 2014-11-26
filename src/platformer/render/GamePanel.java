5c601b71472834b5de88548ee0b356b6f307a26c
adam <adam@adam-aspire-v5>
2014-11-26 17:01:08 -0500

    Initialized the Entity class, meant to be a template for all movable objects to be drawn to the game. Initialized the GamePanel, meant to render all of the games graphics.

---
+++ src/platformer/render/GamePanel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package platformer.render;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author adam
+ */
+public class GamePanel extends JPanel {
+    
+}
