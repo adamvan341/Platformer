182706a83ec1a2ffada803495acf9e2979e28f4c
Adam Vanderneut <adamvan341@gmail.com>
2015-01-12 11:50:10 -0500

    Now randomly generates a level, respects the GameState variable and the Input class.

--- src/platformer/main/GameLoop.java
+++ src/platformer/main/GameLoop.java
@@ -17,85 +17,86 @@
  */
 package platformer.main;
 
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.util.Random;
 import javax.swing.JFrame;
-import platformer.entity.Block;
-import platformer.entity.Entity;
+import platformer.level.Level;
 import platformer.render.GamePanel;
+import platformer.render.Input;
 
 /**
  *
  * @author adam
  */
 public class GameLoop implements Runnable {
+
     boolean gameRunning = true;// Tests if the game is running
-    
-    GamePanel gp;// Graphics are drawn on the game panel
-    public Entity[] e;// A list of all entities in the game
-    
+
+    private GamePanel gPanel;// Graphics are drawn on the game panel
+    private Input input;// Handles mouse and keyboard input
+    public Level level;
+
     public GameLoop() {
-        gp = new GamePanel(640, 480, this);
-        e = new Entity[24];
-        
-        for (int x=0; x<e.length; x++) {
-            try {
-                e[x] = new Block((float) Math.random()*640, (float)Math.random()*480,
-                        ImageIO.read(new File("res/blocks/dirt.png")));
-            } catch (IOException ex) {
-                System.err.println(ex);
-            }
-        }
-        
-        JFrame frame = new JFrame("Platformer");
-        
-        frame.setContentPane(gp);
-        frame.pack();
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
-    
-    /**
-     *  Updates the game logic on a per frame basis.
-     */
-    public void update() {
-        
+	GameState.setGameState(GameState.PLAY_STATE);
+	
+	gPanel = new GamePanel(640, 480, this, input);
+	level = new Level(15, 200);
+	
+	Random r = new Random();
+	for (int x=0; x<level.getWidth(); x++) {
+	    for (int y=0; y<level.getHeight(); y++) {
+		if (r.nextBoolean()) {
+		    level.activateBlock(x, y);
+		}
+	    }
+	}
+	
+	JFrame frame = new JFrame("Platformer");
+	frame.setContentPane(gPanel);
+	frame.pack();
+	frame.setResizable(false);
+	frame.setLocationRelativeTo(null);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
     }
 
     /**
-     *  Opens a gameloop which constantly updates the screen contents and the
-     *  game logic.
+     * Updates the game logic on a per frame basis.
+     */
+    public void update() {
+
+    }
+
+    /**
+     * Opens a game-loop which constantly updates the screen contents and the
+     * game logic.
      */
     @Override
     public void run() {
-        long beforeTime;// Time before updating game logic
-        long afterTime; // Time after updating game logic
-        long deltaTime; // Time between two time checks
-        long sleepTime; // Time to rest until next refresh
-        final long OPTIMAL_TIME = 1000 / 60;
-        
-        while (true) {
-            beforeTime = System.currentTimeMillis();
-            
-            update();
-            gp.repaint();
-            
-            // Calculate how long the thread can sleep
-            afterTime = System.currentTimeMillis();
-            deltaTime = afterTime - beforeTime;
-            sleepTime = OPTIMAL_TIME - deltaTime;
-            // Sleep
-            try {
-                if (sleepTime > 0) {
-                    Thread.sleep(sleepTime);
-                }
-            } catch (InterruptedException ex) {
-                System.err.println(ex);
-            }
-        }
+	long beforeTime;// Time before updating game logic
+	long afterTime; // Time after updating game logic
+	long deltaTime; // Time between two time checks
+	long sleepTime; // Time to rest until next refresh
+	final long OPTIMAL_TIME = 1000 / 60;
+
+	while (true) {
+	    beforeTime = System.currentTimeMillis();
+
+	    update();
+	    gPanel.repaint();
+
+	    // Calculate how long the thread can sleep
+	    afterTime = System.currentTimeMillis();
+	    deltaTime = afterTime - beforeTime;
+	    sleepTime = OPTIMAL_TIME - deltaTime;
+	    // Sleep
+	    try {
+		if (sleepTime > 0) {
+		    Thread.sleep(sleepTime);
+		}
+	    } catch (InterruptedException ex) {
+		System.err.println(ex);
+	    }
+	}
     }
-    
+
 }
